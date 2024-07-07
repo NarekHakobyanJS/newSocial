d1fed7c5ec7b13bd867c4f1917c5e8f65e32fe50
NarekHakobyanJS <haobyan95narek@gmail.com>
2024-07-07 10:54:09 +0400

    captcha use

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -11,50 +11,58 @@ type LoginFormPropsType = {
 }
 
 const LoginForm = ({ autorizationUserLogin }: any) => {
-  return (
-    <Formik
-      initialValues={{
-        login: '',
-        password: '',
-        rememberMe: false
-      }}
-      onSubmit={(values) => autorizationUserLogin(values)}
-    >
-      {
-        ({ values, handleChange, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <div>
-              <Field name='login' />
-            </div>
-            <div>
-              <Field name='password' />
 
-            </div>
-            <div>
-              {/* <Field /> */}
-              <input name='rememberMe' type="checkbox" checked={values.rememberMe} onChange={handleChange} />
-              <span>rememeber me</span>
-            </div>
-            <div>
-              <button type='submit'>login</button>
-            </div>
-          </form>
-        )
-      }
-    </Formik>
+
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          login: '',
+          password: '',
+          rememberMe: false,
+         
+        }}
+        onSubmit={(values) => autorizationUserLogin(values)}
+      >
+        {
+          ({ values, handleChange, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <Field name='login' />
+              </div>
+              <div>
+                <Field name='password' />
+
+              </div>
+              <div>
+
+                
+                <input name='rememberMe' type="checkbox" checked={values.rememberMe} onChange={handleChange} />
+                <span>rememeber me</span>
+              </div>
+              <div>
+                <button type='submit'>login</button>
+              </div>
+            </form>
+          )
+        }
+      </Formik>
+    </>
   )
 }
 const Login = () => {
   const dispatch = useDispatch<any>()
   const auth = useSelector((state: any) => state.auth)
-  
+
 
   const autorizationUserLogin = (user: any) => {
+    
     dispatch(login(user.login, user.password, user.rememberMe))
   }
 
-  if(auth.isAuth){
-    return <Navigate to={`/profile/${auth.userId}`}/>
+  if (auth.isAuth) {
+    return <Navigate to={`/profile/${auth.userId}`} />
   }
   return (
     <div>
--- src/state/authReducer.ts
+++ src/state/authReducer.ts
@@ -1,12 +1,13 @@
-import { authAPI } from "../api/api"
+import { authAPI, securityAPI } from "../api/api"
 
 const SET_USER_DATA = 'auth/SET_USER_DATA'
-
+const GET_CAPTCHA_URL = 'auth/GET_CAPTCHA_URL'
 const initialState = {
     userId: null,
     email: null,
     login: null,
     isAuth: false,
+    captchaUrl : null
 }
 
 const authReducer = (state = initialState, action: any) => {
@@ -16,13 +17,19 @@ const authReducer = (state = initialState, action: any) => {
                 ...state,
                 ...action.payload,
             }
-
+        case GET_CAPTCHA_URL : {
+            return {
+                ...state,
+                captchaUrl : action.payload
+            }
+        }
         default:
             return state
     }
 }
 
 export const setAuthUserData = (userId: number | null, email: string | null, login: string | null, isAuth?: boolean) => ({ type: SET_USER_DATA, payload: { userId, email, login, isAuth } })
+export const getCaptchaUrlSuccess = (url : string) => ({type : GET_CAPTCHA_URL, payload : url})
 
 export const getAuthUserData = () => {
     return async (dispatch: any) => {
@@ -40,12 +47,23 @@ export const login = (email: string, password: string, rememberMe: boolean) => {
     return async (dispatch: any) => {
         const response = await authAPI.login(email, password, rememberMe)
 
-        if (response.data.resultCode === 0) {
+        // if (response.data.resultCode === 0) {
             dispatch(getAuthUserData())
-        }
+        // }
+        // else if(response.data.resultCode === 10) {
+        //     dispatch(getCaptchaUrl())
+        // }
     }
 }
 
+export const getCaptchaUrl = () => {
+    return async (dispatch: any) => {
+        const response = await securityAPI.getCaptchaUrl()
+        const captchaUrl = response.data.url
+            dispatch(getCaptchaUrlSuccess(captchaUrl))
+        
+    }
+}
 
 export const logout = () => {
     return async (dispatch: any) => {
